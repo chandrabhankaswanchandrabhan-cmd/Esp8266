4d212ef02c34a2bc5ba7788fae8515fb25b9e999
chandrabhankaswanchandrabhan-cmd <chandrabhankaswanchandrabhan@gmail.com>
2026-04-25 13:58:26 +0530

    Refactor EEPROM handling and WiFi scanning logic

--- Esp8266.ino
+++ Esp8266.ino
@@ -1,44 +1,34 @@
-#include <ESP8266WiFi.h>
 #include <EEPROM.h>
+#include <ESP8266WiFi.h>
 
-#define MAX_NETWORKS 10  // अधिकतम नेटवर्क की संख्या
-#define EEPROM_SIZE (MAX_NETWORKS * 33)  // हर SSID के लिए 32 कैरेक्टर + 1
+#define MAX_NETWORKS 10
+#define EEPROM_SIZE (MAX_NETWORKS * 33)
 
 void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
+  EEPROM.begin(EEPROM_SIZE);
 
-  EEPROM.begin(EEPROM_SIZE);  // EEPROM शुरू करें
+  Serial.println("पढ़ा जा रहा है डेटा...");
 
-  Serial.println("WiFi स्कैन शुरू हो रहा है...");
-  int n = WiFi.scanNetworks();
+  for (int i = 0; i < MAX_NETWORKS; i++) {
+    int pos = i * 33;
+    char ssid[33];
+    int j = 0;
+    while (EEPROM.read(pos + j) != '\0' && j < 32) {
+      ssid[j] = EEPROM.read(pos + j);
+      j++;
+    }
+    ssid[j] = '\0';  // टर्मिनेटर जोड़ें
 
-  if (n == 0) {
-    Serial.println("कोई नेटवर्क नहीं मिला");
-  } else {
-    Serial.print(n);
-    Serial.println(" नेटवर्क्स पाए गए:");
-    for (int i = 0; i < n && i < MAX_NETWORKS; i++) {
-      String ssid = WiFi.SSID(i);
+    if (ssid[0] != '\0') {
       Serial.print(i + 1);
       Serial.print(": ");
       Serial.print(ssid);
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(" dBm)");
       Serial.println();
-
-      // EEPROM में सेव करें
-      int pos = i * 33;
-      for (int j = 0; j < ssid.length(); j++) {
-        EEPROM.write(pos + j, ssid[j]);
-      }
-      EEPROM.write(pos + ssid.length(), '\0');  // टर्मिनेटर जोड़ें
     }
-    EEPROM.commit();  // बदलाव सेव करें
   }
 }
 
 void loop() {
-  // कोई काम नहीं, बस स्कैन एक बार हुआ
+  // कोई काम नहीं, बस डेटा पढ़ लिया
 }
